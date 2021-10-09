e57e6b5c3bc4f13ac51410162a025f8b9e2adc3d
shseok <gus0674@ajou.ac.kr>
2021-10-10 03:01:12 +0900

    Add main section

--- README.md
+++ README.md
@@ -12,8 +12,19 @@
 - https://ionic.io/ionicons
 
 
-## 미숙
+## 재점검
+- size => 480일 경우 toggle이 없어지는 현상
+
+
+## 미숙한 것들
 
 - block vs inline
 - relative vs absolute
-- 
+- pointer-events: none; 
+    - javascript에서의 mouseover에 대해 작동하지 않게하기 위함
+    - 검사페이지에서도 안잡힘
+- white-space: nowrap;
+- this
+- onclick
+- html table
+- border-collapse
--- hover.js
+++ hover.js
@@ -1,8 +1,22 @@
-let list = document.querySelectorAll('.navigation li');
+// hover된 list item에 클래스 hovered 추가 -> hover 고정
+
+const list = document.querySelectorAll('.navigation li');
 
 function activelink() {
     list.forEach((item) => item.classList.remove('hovered'));
+    // this = 함수 호출 item
     this.classList.add('hovered');
 };
 
 list.forEach((item) => item.addEventListener('mouseover', activelink));
+
+// 메뉴 토글
+
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+const main = document.querySelector('.main');
+
+toggle.onclick = function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
